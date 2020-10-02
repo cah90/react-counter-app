8cfc59ac2b6479942a02e96e88f44a41f1da3911
Cássia <cascrisbern@gmail.com>
2020-10-02 13:05:46 -0300

    add styles. add a styles property on state object and add inline style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = { 
     count: 0,
-    imageUrl: 'https://picsum.photos/200'
+    imageUrl: 'https://picsum.photos/200',
+    styles: {
+      fontSize: 40,
+      fontWeight: "bold"
+    }
    }
 
   render() { 
     return ( 
     <React.Fragment>
       <img src={this.state.imageUrl} alt=""/>
-      <span className="badge badge-primary m-2">{this.formatCount()}</span> 
-      <button className="btn btn-secondary btn-sm">Increment</button>
+      <span style={this.state.styles} className="badge badge-primary m-2">{this.formatCount()}</span> 
+      <button style={ {backgroundColor:"pink"} } className="btn btn-secondary btn-sm">Increment</button>
     </React.Fragment> 
     );
   }
