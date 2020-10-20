2453d13c7b06b246d077f775f4fa8a48e91da828
Cássia <cascrisbern@gmail.com>
2020-10-20 10:24:25 -0300

    updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,7 +11,7 @@ class Counter extends Component {
       fontWeight: "bold"
     },
 
-    tags: ["tag1"]
+    tags: ['tag1']
    }
 
    constructor() {
@@ -24,21 +24,28 @@ class Counter extends Component {
    }
 
    handleIncrement() {
+     this.setState({count:this.state.count+1})
      console.log('clicked', this)
    }
 
   render() { 
     return ( 
     <React.Fragment>
-      <img src={this.state.imageUrl} alt=""/>
-      <span style={this.state.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-      
+    
       {this.state.tags.length === 0 && "Please, create a new tag!"}
       {this.renderTags()}
       <ul> { this.state.tags.map( tag  => <li key={tag} >{ tag }</li>) }</ul>
 
-      <button onClick={ this.handleIncrement } style={ {backgroundColor:"pink"} } className="btn btn-secondary btn-sm">Increment</button>
+      <span style={this.state.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span> 
+      <button 
+        onClick={ () => this.handleIncrement() } 
+        style={ {backgroundColor:"pink"} } 
+        className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       
+      {/* <img src={this.state.imageUrl} alt=""/> */}
     </React.Fragment> 
     );
   }
